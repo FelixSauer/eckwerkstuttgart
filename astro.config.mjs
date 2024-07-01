caf95bdc77c13da42e2c2dfef071bc76f9d3233f
Felix Sauer <felix.sauer@exxeta.com>
2024-07-01 21:54:12 +0200

    Add more_infrastruktur

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,9 @@
 import {defineConfig} from 'astro/config';
 import icon from 'astro-icon';
 
-/*
-import {fileURLToPath} from 'url';
-import path, {dirname} from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-*/
-
 export default defineConfig({
   site: 'https://felixsauer.github.io',
-  base: '',
+  base: 'eckwerkstuttgart',
   integrations: [icon()],
-  /*vite: {
-    resolve: {
-      alias: {
-        '@/': `${path.resolve(__dirname, 'src')}/`
-      }
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `
-          @import "src/styles/GlobalStyling.scss";
-          `,
-        }
-      }
-    }
-  }*/
 });
 
