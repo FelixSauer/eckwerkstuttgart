c785acbe254d8a59fd2eb9b5f698cc4c9e9378ce
Felix Sauer <felix.sauer@exxeta.com>
2024-07-03 18:39:41 +0200

    Add local icons functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import {defineConfig} from 'astro/config';
-import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://felixsauer.github.io',
   base: 'eckwerkstuttgart',
-  integrations: [icon()],
+  integrations: [
+    icon({
+      iconDir: "src/assets/icons",
+    })
+  ]
 });
-
