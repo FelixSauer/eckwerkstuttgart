4b93157d860b98a8545ded46ae980ddb23f25272
Sauer, Felix <exfsauer@emagonline.onmicrosoft.com>
2024-09-25 22:08:19 +0200

    Change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,12 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   /*site: 'https://felixsauer.github.io',*/
-  /*base: 'eckwerkstuttgart',*/
+  base: 'eckwerkstuttgart',
   integrations: [icon({
     iconDir: "src/assets/icons"
   }),
     react({
       experimentalReactChildren: true,
-      include: ['**/react/*'],
-    })]
+    }),
+  ],
 });
