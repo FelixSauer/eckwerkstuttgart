1c4793f5938b1e4d7cd3761156edf0e0d45ada08
Sauer, Felix <exfsauer@emagonline.onmicrosoft.com>
2024-09-25 20:05:31 +0200

    Add add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   /*site: 'https://felixsauer.github.io',
   base: 'eckwerkstuttgart',*/
-  integrations: [
-    icon({
-      iconDir: "src/assets/icons",
-    })
-  ]
+  integrations: [icon({
+    iconDir: "src/assets/icons"
+  }), react()]
 });
