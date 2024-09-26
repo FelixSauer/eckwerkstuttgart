5c9907d3193d807eb045e1f40c4e669de3622da6
Sauer, Felix <exfsauer@emagonline.onmicrosoft.com>
2024-09-26 22:01:57 +0200

    Add tailwide and gallery config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import icon from "astro-icon";
 
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,9 +10,7 @@ export default defineConfig({
   base: 'eckwerkstuttgart',
   integrations: [icon({
     iconDir: "src/assets/icons"
-  }),
-    react({
-      experimentalReactChildren: true,
-    }),
-  ],
+  }), react({
+    experimentalReactChildren: true
+  }), tailwind()]
 });
