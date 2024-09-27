7ae13c014cf14699e6f07318d0622f048735ce95
Sauer, Felix <exfsauer@emagonline.onmicrosoft.com>
2024-09-27 14:40:59 +0200

    Add add images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
 import icon from "astro-icon";
 
 import tailwind from "@astrojs/tailwind";
@@ -7,10 +6,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   /*site: 'https://felixsauer.github.io',*/
-  base: 'eckwerkstuttgart',
+  /*base: 'eckwerkstuttgart',*/
   integrations: [icon({
     iconDir: "src/assets/icons"
-  }), react({
-    experimentalReactChildren: true
   }), tailwind()]
 });
