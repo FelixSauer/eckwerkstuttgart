a655437e93822266cf4da24c1d08214c2af5f7ca
Sauer, Felix <exfsauer@emagonline.onmicrosoft.com>
2024-11-20 10:18:08 +0100

    Add reviews basics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import {ViteImageOptimizer} from "vite-plugin-image-optimizer";
 
 // https://astro.build/config
 export default defineConfig({
-  /*site: 'https://eckwerk-dev.fxsr.de',*/
+  site: 'eckwerk-dev.fxsr.de',
   base: '/',
   integrations: [
     icon({
