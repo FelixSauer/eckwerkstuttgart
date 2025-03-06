62efb7b928d9268a4c1f8878bd084a50e10497e8
Felix Sauer <felix.sauer@emag.com>
2025-03-06 19:15:01 +0100

    Add tailwind import aliases and more

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,52 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontSize: {
-        h1: "2.5rem",
-        h2: "2rem",
-        h3: "1.75rem",
-        h4: "1.5rem",
-        h5: "1.25rem",
-        h6: "1rem",
-      },
-    },
-  },
-  plugins: [],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				montserrat: ['Montserrat Variable', 'sans-serif'],
+				sourceCodePro: ['Source Code Pro Variable', 'monospace'],
+				sourceSans3: ['Source Sans 3 Variable', 'sans-serif'],
+				sourceSansPro: ['Source Sans Pro', 'sans-serif'],
+				roboto: ['Roboto', 'sans-serif']
+			},
+			extend: {
+				keyframes: {
+					slideIn: {
+						'0%': { transform: 'translateX(100vw)' },
+						'100%': { transform: 'translateX(0vw)' }
+					}
+				},
+				animation: {
+					slideIn: 'slideIn 0.5s ease-in'
+				}
+			},
+			colors: {
+				primary: '#828B6F',
+				secondary: '#DBA507',
+				accent: {
+					1: '#8EC7D2',
+					2: '#42AFBF',
+					3: '#FF7180'
+				},
+				dark: '#0D6986',
+				darker: '#07485B',
+				white: '#fafafa',
+				black: '#090909',
+				gray: {
+					1: '#c0c0c0',
+					2: '#b0b0b0',
+					3: '#a0a0a0',
+					4: '#909090',
+					5: '#808080',
+					6: '#707070',
+					7: '#606060',
+					8: '#505050',
+					9: '#404040',
+					10: '#303030'
+				}
+			}
+		}
+	},
+	plugins: []
+}
