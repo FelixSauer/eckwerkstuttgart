77214cbee343415149ee810865e72d69b210e044
Felix <felixsauer82@gmail.com>
2025-06-26 11:52:51 +0200

    Add compress and aria labels on links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,39 +3,54 @@ import icon from 'astro-icon'
 import tailwind from '@astrojs/tailwind'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import sitemap from '@astrojs/sitemap'
+import webmanifest from 'astro-webmanifest';
+import playformCompress from '@playform/compress';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://eckwerkstuttgart.de/',
-	integrations: [
-		icon({
-			iconDir: 'src/assets/icons'
-		}),
-		tailwind(),
-		sitemap({
-			customPages: [
-			'https://eckwerkstuttgart.de/#Home',
-			'https://eckwerkstuttgart.de/#Wohnhandwerk',
-			'https://eckwerkstuttgart.de/#Baumpflege',
-			'https://eckwerkstuttgart.de/#Terrassenbau',
-			'https://eckwerkstuttgart.de/#Kontakt'
-			]
-		})
-	],
-	compressHTML: true,
-	vite: {
-		resolve: {
-			alias: {
-				'@': '/src'
-			}
-		},
-		plugins: [
-			ViteImageOptimizer({
-				webp: {
-					quality: 80,
-					maxSize: 900
-				}
-			})
-		]
-	}
+    site: 'https://eckwerkstuttgart.de/',
+    integrations: [icon({
+        iconDir: 'src/assets/icons'
+		}), tailwind(), sitemap({
+        customPages: [
+        'https://eckwerkstuttgart.de/#Home',
+        'https://eckwerkstuttgart.de/#Wohnhandwerk',
+        'https://eckwerkstuttgart.de/#Baumpflege',
+        'https://eckwerkstuttgart.de/#Terrassenbau',
+        'https://eckwerkstuttgart.de/#Kontakt'
+        ]
+		}), webmanifest({
+        name: 'eckwerkstuttgart.de',
+        icon: 'src/assets/favicon-mode-light.svg',
+        short_name: 'eckwerkstuttgart',
+        description: 'Ihr Partner für Wohnhandwerk, Baumpflege und Terrassenbau in Stuttgart',
+        start_url: '/',
+        theme_color: '#828B6F',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'natural',
+		}), (await import("@playform/compress")).default(), playformCompress()],
+    vite: {
+        resolve: {
+            alias: {
+                '@': '/src'
+            }
+        },
+        plugins: [
+            ViteImageOptimizer({
+                jpg: {
+                    quality: 80,
+                    maxSize: 600
+                },
+                jpeg:	{
+                    quality: 80,
+                    maxSize: 600
+                },
+                webp: {
+                    quality: 80,
+                    maxSize: 600
+                }
+            })
+        ]
+    }
 })
