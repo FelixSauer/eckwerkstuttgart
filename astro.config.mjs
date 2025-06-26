9ec42ee6089567f981e5a544eed278cdafa8d07d
Felix <felixsauer82@gmail.com>
2025-06-26 10:38:00 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,25 @@ import { defineConfig } from 'astro/config'
 import icon from 'astro-icon'
 import tailwind from '@astrojs/tailwind'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL,
+	site: 'https://eckwerkstuttgart.de/',
 	integrations: [
 		icon({
 			iconDir: 'src/assets/icons'
 		}),
-		tailwind()
+		tailwind(),
+		sitemap({
+			customPages: [
+			'https://eckwerkstuttgart.de/#Home',
+			'https://eckwerkstuttgart.de/#Wohnhandwerk',
+			'https://eckwerkstuttgart.de/#Baumpflege',
+			'https://eckwerkstuttgart.de/#Terrassenbau',
+			'https://eckwerkstuttgart.de/#Kontakt'
+			]
+		})
 	],
 	compressHTML: true,
 	vite: {
