36d723c80f9fbd18e5b259865731f63ef72be774
Felix <felixsauer82@gmail.com>
2026-08-30 12:59:02 +0200

    Refactor favicon and manifest files; update site configuration and remove unused assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,42 @@
-import { defineConfig } from 'astro/config'
-import icon from 'astro-icon'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
-import webmanifest from 'astro-webmanifest'
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import webmanifest from "astro-webmanifest";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.eckwerkstuttgart.de/',
+	site: "https://www.eckwerkstuttgart.de/",
 	integrations: [
 		icon({
-			iconDir: 'src/assets/icons'
+			iconDir: "src/assets/icons",
 		}),
 		tailwind(),
 		sitemap({
 			customPages: [
-				'https://www.eckwerkstuttgart.de/#Home',
-				'https://www.eckwerkstuttgart.de/#Wohnhandwerk',
-				'https://www.eckwerkstuttgart.de/#Baumpflege',
-				'https://www.eckwerkstuttgart.de/#Terrassenbau',
-				'https://www.eckwerkstuttgart.de/#Kontakt'
-			]
+				"https://www.eckwerkstuttgart.de/#Home",
+				"https://www.eckwerkstuttgart.de/#Leistungen",
+				"https://www.eckwerkstuttgart.de/#Kontakt",
+			],
 		}),
 		webmanifest({
-			name: 'www.eckwerkstuttgart.de',
-			icon: 'src/assets/favicon-mode-light.svg',
-			short_name: 'eckwerkstuttgart',
-			description: 'Ihr Partner für Wohnhandwerk, Baumpflege und Terrassenbau in Stuttgart',
-			start_url: '/',
-			theme_color: '#828B6F',
-			background_color: '#ffffff',
-			display: 'standalone',
-			orientation: 'natural'
-		})
+			name: "www.eckwerkstuttgart.de",
+			icon: "src/assets/favicon-mode-dark.svg",
+			short_name: "eckwerkstuttgart",
+			description: "Ihr Partner für Handwerk in Stuttgart",
+			start_url: "/",
+			theme_color: "#828B6F",
+			background_color: "#ffffff",
+			display: "standalone",
+			orientation: "natural",
+		}),
 	],
 	vite: {
 		resolve: {
 			alias: {
-				'@': '/src'
-			}
+				"@": "/src",
+			},
 		},
-		plugins: []
-	}
-})
+		plugins: [],
+	},
+});
