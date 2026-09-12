c64ad88220c20db71b791afe7a0970442bdd42e8
Felix <felixsauer82@gmail.com>
2026-09-12 15:26:33 +0200

    update, components, content, images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
@@ -8,6 +9,7 @@ import webmanifest from "astro-webmanifest";
 export default defineConfig({
 	site: "https://www.eckwerkstuttgart.de/",
 	integrations: [
+		mdx(),
 		icon({
 			iconDir: "src/assets/icons",
 		}),
--- src/hooks/navigation.ts
+++ src/hooks/navigation.ts
@@ -4,14 +4,12 @@ import type { INavigation } from '@/types/navigation'
 export async function getNavigation() {
 	const pages = await getCollection('pages')
 
-	const navigation: INavigation[] = pages.map(
-		(page: any): INavigation => ({
-			page: page,
-			title: page.data.title,
-			navigation: page.data.navigation,
-			slug: page.slug
-		})
-	)
+	const navigation: INavigation[] = pages.map((page: any): INavigation => ({
+		page: page,
+		title: page.data.title,
+		navigation: page.data.navigation,
+		slug: page.slug
+	}))
 
 	const navigationHeader = navigation.filter((nav) => nav.navigation?.includes('Header'))
 
